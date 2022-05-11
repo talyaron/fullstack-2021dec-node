ef56dcb0dccef4b4f5792ca6ee3bdd8f341b5ba6
Tal Yaron <tal.yaron@gmail.com>
2022-05-11 19:34:41 +0300

    cont

--- 03-CRUD/Example/server.ts
+++ 03-CRUD/Example/server.ts
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json()); // to get body from client (body = data from client)
 app.use(express.static("public"));
 
 interface User {
   name: string;
   age: number;
+  id: string;
 }
 
-const users: Array<User> = [
-  { name: "Moshe", age: 23 },
-  { name: "Miriam", age: 33 },
-  { name: "Aharon", age: 26 },
+let users: Array<User> = [
+  { name: "Moshe", age: 23, id: "yjnbcsgs" },
+  { name: "Miriam", age: 33, id: "sgfdgdfg" },
+  { name: "Aharon", age: 26, id: "jjghkgutyutyu" },
 ];
 
 app.get("/api/user1", (req, res) => {
@@ -40,6 +42,34 @@ app.get("/api/user3", (req, res) => {
     setTimeout(() => {
       res.send({ user: users[2] });
     }, 10000);
+  } catch (error) {
+    res.send({ error: error.message });
+  }
+});
+
+app.get("/api/get-users", (req, res) => {
+  try {
+    res.send({ users });
+  } catch (error) {
+    res.send({ error: error.message });
+  }
+});
+
+app.delete("/api/delete-user", (req, res) => {
+  try {
+    console.log(req.body);
+    const { userId } = req.body;
+    console.log(userId);
+
+    const index: number = users.findIndex((user) => user.id === userId);
+
+    if (index === -1) throw new Error("Couldnt find user to delete");
+
+    //delete user from users
+    users = users.filter((user) => user.id !== userId);
+    setTimeout(() => {
+      res.send({ users });
+    }, 4000);
   } catch (error) {
     res.send({ error: error.message });
   }
