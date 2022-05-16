0993938f7b6949aaa47ad875b3e914fb0243ca38
Shira1401 <shiraamar44@gmail.com>
2022-05-16 18:49:09 +0300

    array

--- 04-CRUD-Project/Shira/dist/server.js
+++ 04-CRUD-Project/Shira/dist/server.js
@@ -3,9 +3,60 @@ var app = express();
 var port = process.env.PORT || 3000;
 app.use(express.json()); // to get body from client (body = data from client)
 app.use(express.static("public"));
-app.get("/api/get-boxes", function (req, res) {
+var cubeArr = [
+    {
+        id: 1,
+        isCubeFull: 0,
+        isFullX: 0
+    },
+    {
+        id: 2,
+        isCubeFull: 0,
+        isFullX: 0
+    },
+    {
+        id: 3,
+        isCubeFull: 0,
+        isFullX: 0
+    },
+    {
+        id: 4,
+        isCubeFull: 0,
+        isFullX: 0
+    },
+    {
+        id: 5,
+        isCubeFull: 0,
+        isFullX: 0
+    },
+    {
+        id: 6,
+        isCubeFull: 0,
+        isFullX: 0
+    },
+    {
+        id: 7,
+        isCubeFull: 0,
+        isFullX: 0
+    },
+    {
+        id: 8,
+        isCubeFull: 0,
+        isFullX: 0
+    },
+    {
+        id: 9,
+        isCubeFull: 0,
+        isFullX: 0
+    }
+];
+app.post('/api/postShapes', function (req, res) {
     try {
-        res.send({ boxes: boxes });
+        var squreId = req.body.squreId;
+        if (!squreId)
+            throw new Error('squreId is required');
+        renderSymbol(squreId);
+        res.send({ squreArr: squreArr });
     }
     catch (error) {
         res.send({ error: error.message });
--- 04-CRUD-Project/Shira/server.ts
+++ 04-CRUD-Project/Shira/server.ts
@@ -5,13 +5,64 @@ const port = process.env.PORT || 3000;
 app.use(express.json()); // to get body from client (body = data from client)
 app.use(express.static("public"));
 
-app.get("/api/get-boxes", (req, res) => {
-    try {
-      res.send({ boxes });
-    } catch (error) {
-      res.send({ error: error.message });
-    }
-  });
+const cubeArr = [
+  {
+    id:1,
+		isCubeFull: 0,
+		isFullX: 0,
+  },
+  {
+    id:2,
+		isCubeFull: 0,
+		isFullX: 0,
+  },
+  {
+    id:3,
+		isCubeFull: 0,
+		isFullX: 0,
+  },
+  {
+    id:4,
+		isCubeFull: 0,
+		isFullX: 0,
+  },
+  {
+    id:5,
+		isCubeFull: 0,
+		isFullX: 0,
+  },
+  {
+    id:6,
+		isCubeFull: 0,
+		isFullX: 0,
+  },
+  {
+    id:7,
+		isCubeFull: 0,
+		isFullX: 0,
+  },
+  {
+    id:8,
+		isCubeFull: 0,
+		isFullX: 0,
+  },
+  {
+    id:9,
+		isCubeFull: 0,
+		isFullX: 0,
+  }
+]
+
+app.post('/api/postShapes', (req, res) => {
+	try {
+		const { squreId } = req.body;
+		if (!squreId) throw new Error('squreId is required');
+        renderSymbol(squreId);
+        res.send({ squreArr })
+	} catch (error) {
+		res.send({ error: error.message });
+	}
+});
 
 
 
