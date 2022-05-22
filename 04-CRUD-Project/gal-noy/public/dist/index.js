ec870e0d8df7a117c4102b5dc9eb3857592ca429
MartinoySolomon <noy.kakoun@gmail.com>
2022-05-22 19:47:29 +0300

    ,

--- 04-CRUD-Project/gal-noy/public/dist/index.js
+++ 04-CRUD-Project/gal-noy/public/dist/index.js
@@ -34,6 +34,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var GameStats = document.querySelector('gameStats');
 function handleGetTeam1() {
     return __awaiter(this, void 0, void 0, function () {
         var data, play, error, error_1;
@@ -106,3 +107,7 @@ function renderLoader() {
         console.log('remove');
     }
 }
+function build_gameStatistic() {
+    GameStats.innerHTML = "<div class=\"stat_row\"><div>" + gamePlay.TeamA.stat.shots + "</div><div>SHOTS</div><div>" + gamePlay.TeamB.stat.shots + "</div></div>";
+}
+build_gameStatistic();
--- 04-CRUD-Project/gal-noy/public/index.ts
+++ 04-CRUD-Project/gal-noy/public/index.ts
@@ -1,51 +1,84 @@
+const GameStats: HTMLDivElement = document.querySelector('gameStats');
+
+interface GameStats {
+  goals: number;
+  shots: number;
+  shots_on_target: number;
+  possesion: number;
+  passes: number;
+  fouls: number;
+  yellow_cards: number;
+  red_cards: number;
+  offsides: number;
+  corners: number;
+}
+
+interface Game {
+  TeamA: Team;
+  TeamB: Team;
+}
+
+interface Team {
+  name: string;
+  // logo: HTMLImageElement;
+  id: string;
+  stat: GameStats;
+}
 async function handleGetTeam1() {
-    try {
-        console.log("get success")
-      //  @ts-ignore: cannot find module 'axios'
-      const { data } = await axios.get("/api/user1");
-      console.log(data)
-      const { play, error } = data;
-      if (error) throw new Error(error);
-      console.log(play)
-      renderLoader()
-      renderUser(play);
-      console.log("get Team 1 & then get Team 2");
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    console.log("get success")
+    //  @ts-ignore: cannot find module 'axios'
+    const { data } = await axios.get("/api/user1");
+    console.log(data)
+    const { play, error } = data;
+    if (error) throw new Error(error);
+    console.log(play)
+    renderLoader()
+    renderUser(play);
+    console.log("get Team 1 & then get Team 2");
+  } catch (error) {
+    console.error(error);
   }
-  
-  
-  async function handleGetTeam2() {
-    try {
+}
+
+
+async function handleGetTeam2() {
+  try {
     //@ts-ignore: cannot find module 'axios'
-      const { data } = await axios.get("/api/user2");
-      console.log(data)
-      const { play, error } = data;
-      if (error) throw new Error(error);
-      console.log(play)
-      renderLoader()
-      renderUser(play);
-      console.log("get Team 2 after you get Team 1");
-    } catch (error) {
-      console.error(error);
-    }
+    const { data } = await axios.get("/api/user2");
+    console.log(data)
+    const { play, error } = data;
+    if (error) throw new Error(error);
+    console.log(play)
+    renderLoader()
+    renderUser(play);
+    console.log("get Team 2 after you get Team 1");
+  } catch (error) {
+    console.error(error);
   }
-  
+}
+
 
 function renderUser(play: Team) {
-    const root: HTMLElement = document.querySelector("#root");
-  
-    root.innerText = `user ${play.name} is ${play.id} years old`;
-  }
+  const root: HTMLElement = document.querySelector("#root");
 
-  function renderLoader(){
-    const loader: HTMLElement = document.querySelector('#loader')
-    if(!loader.classList.contains('lds-dual-ring')){
-        loader.classList.add('lds-dual-ring');
-        console.log('add')
-    } else {
-        loader.classList.remove('lds-dual-ring');
-        console.log('remove')
-    }
+  root.innerText = `user ${play.name} is ${play.id} years old`;
 }
+
+function renderLoader() {
+  const loader: HTMLElement = document.querySelector('#loader')
+  if (!loader.classList.contains('lds-dual-ring')) {
+    loader.classList.add('lds-dual-ring');
+    console.log('add')
+  } else {
+    loader.classList.remove('lds-dual-ring');
+    console.log('remove')
+  }
+}
+
+
+function build_gameStatistic() {
+  GameStats.innerHTML = `<div class="stat_row"><div>${gamePlay.TeamA.stat.shots}</div><div>SHOTS</div><div>${gamePlay.TeamB.stat.shots}</div></div>`
+}
+
+build_gameStatistic();
