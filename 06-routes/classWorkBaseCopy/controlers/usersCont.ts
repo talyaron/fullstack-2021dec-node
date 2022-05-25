05eb37e9a6a60c737b6f1812f833c9f4aa21ba80
hanochshachar <goralot550@gmail.com>
2022-05-25 20:39:09 +0300

    hh

--- 06-routes/classWorkBaseCopy/controlers/usersCont.ts
+++ 06-routes/classWorkBaseCopy/controlers/usersCont.ts
@@ -1,8 +1,20 @@
-import User from "../model/usersModel";
+
+interface User{
+  username:string;
+  id:string;
+}
+const users:Array<User>=[
+  {username:'Moshe', id:'dgdsg'},
+  {username:'Miriam,', id:'dgdsdghghg'},
+]
+
+export async function getAllUsers(req, res) {
+  try {
 
 
-export async function getAllUsers
-
-
-
-  export const someFunction = (y)=>{return y*2};
+    res.send({ ok: true, users });
+} catch (error) {
+    console.log(error.error);
+    res.send({ error: error.message });
+}
+}
--- 06-routes/classWorkBaseCopy/server.ts
+++ 06-routes/classWorkBaseCopy/server.ts
@@ -1,31 +1,35 @@
 import express from "express";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 const app = express();
 const port: number = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
 
-interface User{
-  username:string;
-  id:string;
-}
-const users:Array<User>=[
-  {username:'Moshe', id:'dgdsg'},
-  {username:'Miriam,', id:'dgdsdghghg'},
-]
+// interface User{
+//   username:string;
+//   id:string;
+// }
+// const users:Array<User>=[
+//   {username:'Moshe', id:'dgdsg'},
+//   {username:'Miriam,', id:'dgdsdghghg'},
+// ]
 
 
-app.get('/users/get-all-users',(req, res)=> {
-  try {
+// app.get('/users' 'get-users', (req, res)=> {
+//   try {
 
 
-      res.send({ ok: true, users });
-  } catch (error) {
-      console.log(error.error);
-      res.send({ error: error.message });
-  }
-})
+//       res.send({ ok: true, users });
+//   } catch (error) {
+//       console.log(error.error);
+//       res.send({ error: error.message });
+//   }
+// })
+
+import usersRoute from "./route/usersRout";
+app.use("/users", usersRoute);
+
 
 
 app.listen(port, () => {
