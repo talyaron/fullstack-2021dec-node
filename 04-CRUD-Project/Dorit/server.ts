5d8b378b5520579a8dd12ca55aec58c58935e43c
doritgy <63942750+doritgy@users.noreply.github.com>
2022-05-25 19:06:20 +0300

    fix

--- 04-CRUD-Project/Dorit/server.ts
+++ 04-CRUD-Project/Dorit/server.ts
@@ -16,14 +16,14 @@ interface Recipe {
     { name: "HoneyCake", 
     ingredients:["honey 30 gr","sugar 0.5 glass","flower 2 glasses","eggs 3"],
      prepareMode: ["put into a bowel all the ingredients","stir until very smooth","put on the oven on 175 degrees","bake for 45 minutes"],
-     adderName:"Dorit Guy"}
+     adderName:"Dorit Guy"},
      { name: "vanillaCake", 
      ingredients:["vanilla 30 gr","sugar 0.5 glass","flower 2 glasses","eggs 3"],
       prepareMode: ["put into a bowel all the ingredients","stir until very smooth","put on the oven on 175 degrees","bake for 45 minutes"],
       adderName:"Gadi Guy"}
   ];
 //route
-app.put("/api/get-cake", (req, res) => {
+app.put("/api/get-recipe", (req, res) => {
     try {
         const {recipeName} = req.body
         if (!recipeName) throw new Error("recipeName is required");
@@ -49,6 +49,28 @@ app.post('/api/add-recipe', (req, res) => {
     recipes.push(myRecipe);
 
     res.send({myRecipe});
+
+  } catch (error) {
+    res.send({ error: error.message });
+  }
+})
+
+app.post('/api/check-recipe', (req, res) => {
+  try {
+    const {adderName, recipeName } = req.body;
+    if(!adderName) throw new Error("adderName is required");
+    if(!recipeName) throw new Error("recipeName is required");
+    let myRecipe:any = {recipeName, adderName};
+    const recipeIndex = recipes.findIndex(recipe => recipe.name === myRecipe.recipeName);
+    if (recipeIndex===-1) throw new Error("recipeName not found")
+    const recipeAdderName = recipes[recipeIndex]['adderName'];
+    console.debug(recipeAdderName,recipes[recipeIndex]);
+    if (recipeAdderName!==myRecipe.adderName){
+      throw new Error("This recipe was added by another user")
+    }else{
+      myRecipe=recipes[recipeIndex]
+      res.send({myRecipe})
+    }
 
   } catch (error) {
     res.send({ error: error.message });
