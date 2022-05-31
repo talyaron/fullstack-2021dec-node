8f07a9577d9301854752e526d87cb533d45d96b6
Bella-UI <bellabrailovsky@gmail.com>
2022-05-31 19:03:14 +0300

    ...

--- 04-CRUD-Project/Bella/server.ts
+++ 04-CRUD-Project/Bella/server.ts
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.static('public'))
+
 
 //all users array
 let users: Array < user > = [];
@@ -111,5 +112,5 @@ const uid = function () {
 }
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-});
+    return console.log(`Server is listening at http://localhost:${port}`);
+  });
