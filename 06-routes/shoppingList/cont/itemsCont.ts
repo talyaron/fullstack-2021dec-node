1658ed13efb64741af4ab501a016a9b8808ddf00
Tal Yaron <tal.yaron@gmail.com>
2022-06-01 18:08:50 +0300

    indentetion

--- 06-routes/shoppingList/cont/itemsCont.ts
+++ 06-routes/shoppingList/cont/itemsCont.ts
@@ -1,24 +1,22 @@
-import { Item } from '../model/itemModel'
+import { Item } from "../model/itemModel";
 
-const items:Array<Item> = [
-    {
-        name: 'Milk',
-        itemId: '123MI',
-        bought: false,
-        userId: 'abcd'
-    },
-    {
-        name: 'Bread',
-        itemId: '123BR',
-        bought: false,
-        userId: 'abc'
-    }
+const items: Array<Item> = [
+  {
+    name: "Milk",
+    itemId: "123MI",
+    bought: false,
+    userId: "abcd",
+  },
+  {
+    name: "Bread",
+    itemId: "123BR",
+    bought: false,
+    userId: "abc",
+  },
 ];
-export async function HandleUpdateItem(req, res){
-try {
-    const {name} = req.body
-    console.log(name)
-} catch (error) {
-    
-}
+export async function HandleUpdateItem(req, res) {
+  try {
+    const { name } = req.body;
+    console.log(name);
+  } catch (error) {}
 }
