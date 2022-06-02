e128a452d5db869e8daeabd9af0ed746502d496d
Bella Brail <bellabrailovsky@gmail.com>
2022-06-02 18:06:36 +0300

    ...

--- 06-routes/shoppingList/routes/dist/usersRoute.js
+++ 06-routes/shoppingList/routes/dist/usersRoute.js
@@ -3,22 +3,9 @@ exports.__esModule = true;
 var express_1 = require("express");
 var router = express_1["default"].Router();
 var usersCont_1 = require("../cont/usersCont");
-<<<<<<< HEAD
-router.get('/api/get-users', usersCont_1.getUsers);
-exports["default"] = router;
-// import { User } from "../model/userModel";
-// {
-//   getUsers
-//   // addUser,
-//   // deleteUser,
-// }
-// from "../controlers/usersCont";
-// import {getUsers} from "../cont/usersCont"
-//   router.get('/api/get-users',getUsers)
-// export default router;
-=======
+//
 router
     .get("/get-users", usersCont_1.getAllUsers)
-    .patch("/update-user", usersCont_1.updateUser);
+    .patch("/update-user", usersCont_1.updateUser)["delete"]("/user-delete", usersCont_1.handleDeleteUser)
+    .post("/user-add", usersCont_1.handleAddUser);
 exports["default"] = router;
->>>>>>> group1
