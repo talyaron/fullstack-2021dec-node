3abdafd0c2764efca44dec5403e606d31a0fa925
Bella Brail <bellabrailovsky@gmail.com>
2022-06-03 17:53:40 +0300

    userRoute duplication fix

--- 06-routes/shoppingList/routes/dist/usersRoute.js
+++ 06-routes/shoppingList/routes/dist/usersRoute.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 var express_1 = require("express");
 var router = express_1["default"].Router();
-var usersCont_1 = require("../cont/usersCont");
+var usersCont_1 = require("../../cont/usersCont");
 //
 router
     .get("/get-users", usersCont_1.getAllUsers)
