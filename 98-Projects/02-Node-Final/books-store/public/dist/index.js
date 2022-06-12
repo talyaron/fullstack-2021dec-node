acb3e2cdd0dfbd39d890c8f4ad75c0e81d54f12d
hanochshachar <goralot550@gmail.com>
2022-06-12 20:07:35 +0300

    gg

--- 98-Projects/02-Node-Final/books-store/public/dist/index.js
+++ 98-Projects/02-Node-Final/books-store/public/dist/index.js
@@ -34,23 +34,30 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var html = "";
 function handleUpBook(ev) {
     return __awaiter(this, void 0, void 0, function () {
-        var image, name, description, price, data;
+        var image, name, description, price, data, error_1;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
                     ev.preventDefault();
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 3, , 4]);
                     image = ev.target.element.bookImg.value;
                     name = ev.target.element.namOfBook.value;
                     description = ev.target.element.description.value;
                     price = ev.target.element.price.value;
                     return [4 /*yield*/, axios.post('/booksStore', image, name, description, price)];
-                case 1:
+                case 2:
                     data = (_a.sent()).data;
                     console.log(data);
-                    return [2 /*return*/];
+                    return [3 /*break*/, 4];
+                case 3:
+                    error_1 = _a.sent();
+                    console.log(error_1);
+                    return [3 /*break*/, 4];
+                case 4: return [2 /*return*/];
             }
         });
     });
@@ -61,7 +68,7 @@ function postBook(book) {
         return __generator(this, function (_a) {
             books = document.querySelector("#books");
             html = "";
-            html = "<img src=\"" + book.bookImage + "\" id=\"bookImg\"> <br>\n   <h3>" + book.name + "</h3> <br>\n   <p>" + book.description + "</p> <br>\n   <h1>" + book.price + "</h1> <br>";
+            html = "<img src=\"" + book.bookImage + "\" id=\"bookImg\"> <br>\n   <h3>" + book.name + "</h3> <br> <br>\n   <p>" + book.description + "</p> <br> <br>\n   <h1>" + book.price + "</h1> <br> <br> <br>";
             books.innerHTML = html;
             return [2 /*return*/];
         });
--- 98-Projects/02-Node-Final/books-store/public/index.ts
+++ 98-Projects/02-Node-Final/books-store/public/index.ts
@@ -1,8 +1,11 @@
 
 
-let html = "";
+
 async function handleUpBook(ev){
    ev.preventDefault()
+   try {
+      
+   
    const image = ev.target.element.bookImg.value
    const name = ev.target.element.namOfBook.value
    const description = ev.target.element.description.value
@@ -14,7 +17,9 @@ async function handleUpBook(ev){
    //@ts-ignore
    const {data} = await axios.post('/booksStore', image, name, description, price)
    console.log(data)
-
+} catch (error) {
+   console.log(error)     
+}
 
 }
 
@@ -22,9 +27,9 @@ async function postBook(book) {
    const books = document.querySelector("#books")
    let html = "";
    html = `<img src="${book.bookImage}" id="bookImg"> <br>
-   <h3>${book.name}</h3> <br>
-   <p>${book.description}</p> <br>
-   <h1>${book.price}</h1> <br>`
+   <h3>${book.name}</h3> <br> <br>
+   <p>${book.description}</p> <br> <br>
+   <h1>${book.price}</h1> <br> <br> <br>`
 
    books.innerHTML = html
    
