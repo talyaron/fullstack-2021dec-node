0327150bd84716e7a365e09902cf0b7b985103f9
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-06-14 03:23:56 +0300

    ax-meir-fun

--- 02-routes-async/followups/meir/public/app.ts
+++ 02-routes-async/followups/meir/public/app.ts
@@ -1,1 +1,11 @@
-alert("#conect");
+function handleGetUser1(){
+    
+    try {
+        console.log('get user 4');
+        axios.get('/api/user1').then(results=>{
+            console.log(results);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
