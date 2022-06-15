28f262a79dd9d4c23076c0c02d84d2631ee1a937
Roy Felhiemer <roy0507689500@gmail.com>
2022-06-15 18:06:47 +0300

    23

--- 98-Projects/02-Node-Final/roy/cont/MTACont.ts
+++ 98-Projects/02-Node-Final/roy/cont/MTACont.ts
@@ -29,3 +29,65 @@ export const handleDeleteTeam = (req, res) => {
 		res.send({ error: error.message });
 	}
 };
+
+export interface transfer{
+    headline:string,
+    Id:string,
+	photo:string,
+    url:string,
+    text:string,
+}
+let transfers: Array<transfer> = [ { headline: 'רועי עמדי חתם ל-5 שנים במכבי', Id: 'a',photo:'https://static.maccabi-tlv.co.il/wp-content/uploads/2022/06/MTA_202206141144252afa12fd4f15e1ddfb888d96d0adda08.jpg', url: 'https://www.maccabi-tlv.co.il/2022/06/%d7%a8%d7%95%d7%a2%d7%99-%d7%a2%d7%9e%d7%93%d7%99-%d7%97%d7%aa%d7%9d-%d7%9c-5-%d7%a9%d7%a0%d7%99%d7%9d-%d7%91%d7%9e%d7%9b%d7%91%d7%99/', text: 'רועי עמדי, (18) חתם על חוזה חדש במכבי תל אביב כשהשוער המתנשא לגובה 194 ס”מ הבטיח את עתידו עד תום עונת 2026/27.' }, 
+{ headline: 'יוריס ואן אובריים חתם ל-3 שנים במכבי', Id: 'ab', photo:'https://static.maccabi-tlv.co.il/wp-content/uploads/2022/06/MTA_202206130400200f86499f19313c266bc8c370fdc62904.jpg', url: 'https://www.maccabi-tlv.co.il/2022/06/%d7%99%d7%95%d7%a8%d7%99%d7%a1-%d7%95%d7%90%d7%9f-%d7%90%d7%95%d7%91%d7%a8%d7%99%d7%99%d7%9d-%d7%97%d7%aa%d7%9d-%d7%9c-3-%d7%a9%d7%a0%d7%99%d7%9d-%d7%91%d7%9e%d7%9b%d7%91%d7%99/', text:'הקשר ההולנדי בן ה-28 מצטרף למכבי מאוטרכט לאחר שחתם על חוזה ל-3 שנים.' },
+{ headline: 'ולאדן איביץ מונה למאמן מכבי תל אביב', Id: 'abב', photo:'https://static.maccabi-tlv.co.il/wp-content/uploads/2022/06/MTA_2022061205273713384f96102bb70bcb480140a7ea2cd5.jpg', url: 'https://www.maccabi-tlv.co.il/2022/06/%d7%95%d7%9c%d7%90%d7%93%d7%9f-%d7%90%d7%99%d7%91%d7%99%d7%a5-%d7%9e%d7%95%d7%a0%d7%94-%d7%9b%d7%9e%d7%90%d7%9e%d7%9f-%d7%9e%d7%9b%d7%91%d7%99/', text:'איביץ חוזר למכבי לשתי עונות נוספות ויעזר במילוש וסלינוביץ, סינישה גוגיץ כעוזרי מאמן, חריסטוס קלפקיס כמאמן השוערים ובמאמן הכושר פבלוס גוציס' } ];
+
+
+
+export async function getAllTransfers(req, res) {
+	try {
+		res.send({ transfers });
+	} catch (error) {
+		console.log('transfers array not valid');
+		res.send({ error: error.message });
+	}
+}
+export interface score{
+    fTeamSymbol:string,
+	sTeamSymbol:string,
+	id:string,
+	fTeamScore:string,
+	sTeamScore:string,
+}
+
+let score: Array<score> = [ { fTeamSymbol: 'https://upload.wikimedia.org/wikipedia/he/4/45/Maccabi_Tel_Aviv_FC.png', sTeamSymbol: 'https://upload.wikimedia.org/wikipedia/he/thumb/5/52/Hapoel_Tel_Aviv_Logo.png/640px-Hapoel_Tel_Aviv_Logo.png',
+                              id:'a', fTeamScore: '3', sTeamScore: '0' }]
+
+export async function getScore(req, res) {
+	try {
+		res.send({ score });
+	} catch (error) {
+		console.log('score array not valid');
+		res.send({ error: error.message });
+	}
+}
+
+export interface article{
+    headline:string,
+    Id:string,
+	photo:string,
+    url:string,
+    text:string,
+}
+
+let articles: Array<article> = [ { headline: 'מתן בלטקסה סיכם לשלוש שנים באוסטריה וינה, צפוי לחתום במועדון', Id: 'b',photo:'https://sport1images.maariv.co.il/image/upload/1099154', url: 'https://sport1.maariv.co.il/israeli-soccer/israelis-abroad/article/898010/', text: 'הבלם שסיים את חוזהו במכבי תל אביב כבר הגיע לבירת אוסטריה והשלים את הבדיקות הרפואיות לקראת הצטרפותו לקבוצה שסיימה את העונה במקום השלישי ותתמודד בפלייאוף הליגה האירופית.' }, 
+{ headline: 'האקדמאי: מה יביא איתו יוריס ואן אובריים למכבי תל אביב?', Id: 'bc', photo:'https://sport1images.maariv.co.il/image/upload/1099093', url: 'https://sport1.maariv.co.il/magazine/article/897901/', text:'יוריס ואן אובריים חובב מתמטיקה ופיזיקה, מתעמק במשבר האקלים וצופה במשך שעות בכדורגל מכל העולם. האהבה שלו לעולמות חדשים הביאה אותו עד מכבי תל אביב ואחרי שביסס את מעמדו כשחקן לגיטימי בליגה ההולנדית, הוא ינסה להפוך לאינייסטה של ולאדן איביץ' },
+{ headline: 'דסה: "מעריך את ההצעה ממכבי ת"א, אבל המטרה שלי להתקדם"', Id: 'cd', photo:'https://sport1images.maariv.co.il/image/upload/1099014', url: 'https://sport1.maariv.co.il/israeli-soccer/ligat-haal/article/897626/', text:'מגן נבחרת ישראל הודה: "מכבי תל אביב יודעת כמה אני אוהב ומכבד אותה, אבל בקרוב תדעו על עתידי ותהיו שמחים בשבילי". גם דור פרץ מסרב כרגע להתחייב לצהובים' } ];
+
+export async function getAllArticales(req, res) {
+	try {
+		res.send({ articles });
+	} catch (error) {
+		console.log('articles array not valid');
+		res.send({ error: error.message });
+	}
+}
--- 98-Projects/02-Node-Final/roy/routes/MTARoutes.ts
+++ 98-Projects/02-Node-Final/roy/routes/MTARoutes.ts
@@ -2,12 +2,18 @@ import express from "express";
 const router = express.Router();
 import {
   getAllTeams,
-  handleDeleteTeam
+  handleDeleteTeam,
+  getAllTransfers,
+  getScore,
+  getAllArticales
 } from "../cont/MTACont";
 
 //
 router
   .get("/get-teams", getAllTeams)
   .delete("/Team-delete", handleDeleteTeam)
+  .get("/get-transfers", getAllTransfers)
+  .get("/get-score", getScore)
+  .get("/get-articles", getAllArticales)
 
 export default router;
