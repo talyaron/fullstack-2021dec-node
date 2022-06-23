58580ef1afbf08984ab6ebde528f783b9fb560a7
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-06-24 00:07:01 +0300

    sd

--- 98-Projects/02-Node-Final/Gal-tasksBar/public/index.ts
+++ 98-Projects/02-Node-Final/Gal-tasksBar/public/index.ts
@@ -180,3 +180,18 @@ async function handleSearchItems(event) {
 
 
 
+export function getUserId(): string | false {
+    try {
+      const queryString = window.location.search;
+      console.log(queryString);
+  
+      const urlParams = new URLSearchParams(queryString);
+  
+      const userId = urlParams.get("userId");
+      console.log(userId);
+      return userId;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
