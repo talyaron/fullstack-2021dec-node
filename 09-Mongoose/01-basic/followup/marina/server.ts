cbbae2a00875a6c24199879dffbb28b5e6c60496
Marina <garpalion@gmail.com>
2022-06-26 16:28:50 +0300

    hw

---
+++ 09-Mongoose/01-basic/followup/marina/server.ts
@@ -0,0 +1,24 @@
+console.log("Yes! Connected!");
+
+import mongoose from "mongoose";
+import express from "express";
+const app = express();
+const port = process.env.PORT || 4009;
+app.use(express.static("public"));
+app.use(express.json());
+
+import routePaint from './routes/routePaint';
+app.use('/art', routePaint);
+
+mongoose.connect(
+  "mongodb+srv://ChicZiv:R7ezBHsv3EIXqWa5@cluster0.nbjog.mongodb.net/art-painting?retryWrites=true&w=majority"
+);
+console.log("Connected to DB!");
+
+app.listen(port, () => {
+  console.log(`Express is listening at ${port}`);
+});
+
+
+
+
