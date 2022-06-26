ecdd07a0866a73359ac5f45acba59161360406b5
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-06-26 18:30:02 +0300

    dsf

--- 09-Mongoose/01-basic/homeTask1/gal/public/index.ts
+++ 09-Mongoose/01-basic/homeTask1/gal/public/index.ts
@@ -1,3 +1,41 @@
+interface Cat{
+    name: string,
+    age: number
+}
+
+
+let allCats:Array<Cat>=[];
+
+
+async function getAllCats():Promise<any>{
+try{
+   //@ts-ignore
+  const { data } = await axios.get("/cats/get-all-cats")
+    console.log(data);
+    const {allCats,error} = data;
+    if(error) throw error;
+    renderAllCats(allCats);
+
+}catch(error) {
+  console.error(error);
+}
+}
+
+function renderAllCats(allCats){
+    try {
+        if(!Array.isArray(allCats)) throw new Error ('the cats are not array');
+        let newCat: HTMLDivElement = document.querySelector('#root')
+        let html:string="";
+        allCats.forEach((Element)=>{
+            html += `<p>name:${Element.name}, age:${Element.age}' /></p>`;
+    });
+    newCat.innerHTML = html;
+    }catch(error) {
+        console.error(error);
+      }
+    }
+
+
 async function handleAddCat(ev:any) {
     try {
         ev.preventDefault();
@@ -8,7 +46,13 @@ async function handleAddCat(ev:any) {
         const {data} = await axios.post("/cats/add-cat",{name,age});
         console.log(data)
         console.dir(data)
+        renderCats(data);
     }catch (error) {
         console.error(error);
       }
 }
+
+function renderCats(cat){
+    const catR = document.querySelector("#root")
+    catR.innerHTML=`new cat: name=${cat.name}, age:${cat.age}}`
+ }
--- 09-Mongoose/01-basic/homeTask1/gal/routes/route.ts
+++ 09-Mongoose/01-basic/homeTask1/gal/routes/route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 
-import { addCat } from "../cont/cont";
+import { addCat,getAllCats } from "../cont/cont";
 
 router.post("/add-cat", addCat)
+router.get("/get-all-cats", getAllCats)
 
 export default router
