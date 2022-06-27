e117294e41e222cf27f2be16dbb5e9c9dbdeeef2
hanochshachar <goralot550@gmail.com>
2022-06-27 18:50:07 +0300

    tried to fix the array

--- 09-Mongoose/01-basic/homeTask1/hanoch/server.ts
+++ 09-Mongoose/01-basic/homeTask1/hanoch/server.ts
@@ -2,8 +2,21 @@ import  express  from "express";
 import mongoose from "mongoose";
 
 const app = express()
+const port = 3000;
  
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://username:password@host:port/database?options...')
+mongoose.connect('mongodb+srv://hanoch:Wih5HKHggXATg2Bc@cluster0.37kenwy.mongodb.net/catsex?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("connect to DB");
+    
+}).catch((err)=> console.log(err))
+
+import catsRout from './routs/catsRout';
+app.use('/cat', catsRout)
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+    
+} )
