28edf75345c1c51dbb7d3263c48ef458a69bf1f3
Bella Brail <bellabrailovsky@gmail.com>
2022-07-03 20:01:19 +0300

    ...

--- 09-Mongoose/02-register-login/challange2/group2/routes/dist/userRoute.js
+++
@@ -1,6 +0,0 @@
-// import express from 'express';
-// import { pushUser} from '../controllers/userCont';
-// const router = express.Router();
-// router
-// .post('/push-User', pushUser)
-// export default router
