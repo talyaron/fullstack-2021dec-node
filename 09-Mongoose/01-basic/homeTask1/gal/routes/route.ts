76044b798f680bee36dbe83e50f92a46831f501e
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-07-04 16:45:20 +0300

    jk

--- 09-Mongoose/01-basic/homeTask1/gal/routes/route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { addCat,getAllCats } from "../cont/cont";
-
-router.post("/add-cat", addCat)
-router.get("/get-all-cats", getAllCats)
-
-export default router
