ab625b3eddfd289e7a9980328d3371cc3dfb9da4
Marina <garpalion@gmail.com>
2022-07-06 20:33:30 +0300

    del

--- 08-cookies/followups-simple/06.07.2022/routes/dist/clientRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var router = express_1["default"].Router();
-var clientController_1 = require("../controllers/clientController");
-router.get("/hello", clientController_1.setHello);
-exports["default"] = router;
