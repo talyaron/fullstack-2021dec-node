7755d74e075fd76f42cb5884b763a5624a54c521
galgross1 <97188215+galgross1@users.noreply.github.com>
2022-07-17 19:32:38 +0300

    dsfg

--- 09-Mongoose/04-JWT-17-7-22/fllowups/gal/Conts/UserCont.ts
+++ 09-Mongoose/04-JWT-17-7-22/fllowups/gal/Conts/UserCont.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import UserModel from "../Models/UserModels";
+import jwt from 'jwt-simple';
 
 export async function register(req, res) {
   try {
@@ -20,8 +21,15 @@ export async function login(req, res) {
     const userDB = await UserModel.findOne({ email, password });
     if (!userDB) throw new Error("User name or password do not match");
 
-    //send cookie
+      
+    const role = userDB.role || 'user';
+    console.log(role)
 
+    //send cookie
+    const cookie= {userId: userDB._id, role};
+    const secret = process.env.JWT_SECRET
+    const jwtCode = jwt.encode(cookie, secret);
+    
     res.cookie('user', {userId: userDB._id},{maxAge:1000*60*4})
     
     res.send({ ok: true });
