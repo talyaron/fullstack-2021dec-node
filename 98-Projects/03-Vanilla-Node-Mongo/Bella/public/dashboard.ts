90ec05398e722a17316f57741640b27a3c781643
Bella Brail <bellabrailovsky@gmail.com>
2022-07-31 17:29:04 +0300

    Implementing ChartJS

--- 98-Projects/03-Vanilla-Node-Mongo/Bella/public/dashboard.ts
+++ 98-Projects/03-Vanilla-Node-Mongo/Bella/public/dashboard.ts
@@ -1,55 +1,60 @@
-function handleLoad() {
-    try {
-        // //@ts-ignore const { data } = await axios.get("/data/get-all-data");
-        // console.log(data); stocksAPI()
+// import Chartjs, { Chart } from 'chart.js';
 
-    } catch (error) {
-        console.error(error)
+let employeeLabel = [], employeeSalaryData = [], employeeAgeData = []
+
+async function dummyChart() {
+  await getDummyData()
+
+const ctx = document.getElementById('myChart');
+
+const chart = new Chart(ctx, {
+    // The type of chart
+    type: 'bar',
+
+    // The data for the dataset
+    data: {
+        labels: employeeLabel,
+        datasets: [{
+            label: 'Employee Salary',
+            backgroundColor: 'blue',
+            borderColor: 'rgb(255, 99, 132)',
+            data: employeeSalaryData
+        },
+        {
+          label: 'Employee Age',
+          backgroundColor: 'pink',
+          borderColor: 'rgb(255, 99, 132)',
+          data: employeeAgeData
+      }
+      ]
+    },
+
+    // Configuration options
+    options: {
+      tooltips: {
+        mode: 'index'
+      }
     }
+})}
+
+dummyChart()
+
+
+//Fetch Data from API
+
+async function getDummyData() {
+  const apiUrl = "http://dummy.restapiexample.com/api/v1/employees"
+
+  const response = await fetch(apiUrl)
+  const barChatData = await response.json()
+  
+  const salary = barChatData.data.map((obg) => obg.employee_salary)
+  console.log(salary)
+  const age = barChatData.data.map((obg) => obg.employee_age)
+  const name = barChatData.data.map((obg) => obg.employee_name)
+
+ employeeSalaryData = salary
+ employeeAgeData = age
+ employeeLabel = name
 }
 
-async function stocksAPI(ev) {
-    try {
-        ev.preventDefault();
-
-        const userInput = ev.target.elements.userInput.value;
-        console.log(userInput);
-        //@ts-ignore
-
-        const {data} = await axios.get(`https://cloud.iexapis.com/stable/tops?token=pk_421d8331d521478798685db9b5be24fa&symbols=${userInput}`);
-        console.log(data);
-        render(data)
-
-
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-function render(data) {
-            
-    const root = document.querySelector("#root");
-    root.innerHTML = `<h3>${data}</h3>`;
-}
-
-async function getUserByCookie() {
-    try{
-   
-   
-    //@ts-ignore
-    const { data } = await axios.get("/users/get-user");
-    console.log(data);
-    const {user} = data;
-    if(!user){
-        throw new Error('User not found');
-    }
-
-    const usernameDB = user.username;
-    const root = document.getElementById('root');
-    if(root){
-        root.innerHTML = `<h1>Welcome ${usernameDB}</h1>`
-    }
-    } catch (error) {
-        console.error(error)
-    }
-}
