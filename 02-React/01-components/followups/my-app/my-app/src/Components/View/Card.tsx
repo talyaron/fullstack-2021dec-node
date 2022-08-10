ee33d71db55fb0a6a447265cad245a7c25769cd8
Gal <galgross24@gmail.com>
2022-08-10 19:47:20 +0300

    sdf

---
+++ 02-React/01-components/followups/my-app/my-app/src/Components/View/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface CardProps{
+    title:string;
+    src:string;
+};
+
+const Card = (props:CardProps) =>{
+    return(
+    <>
+    
+    </>
+};
+
+export default Card;
