a0fd3c7b574e1281dcd88f149a56f4be1b98df71
Bella Brail <bellabrailovsky@gmail.com>
2022-08-10 20:55:12 +0300

    fun style

--- 02-React/01-components/followups/Adam-Bella/my-app/src/App.tsx
+++ 02-React/01-components/followups/Adam-Bella/my-app/src/App.tsx
@@ -3,26 +3,20 @@ import logo from './logo.svg';
 import './design/styleGuides/app.scss';
 import Card from './design/components/cards/Card';
 
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <div className="btn">OK</div>
-        <Card heading='a' tag='This is a tag'/>
-        <Card heading='b'tag='This is a tag 2'/>
-        <Card heading='c' tag='This is a tag 3'/>
-        <Card heading='d' tag='This is a tag'/>
-        <Card heading='e' tag='This is a tag'/>
-        <button>OK2</button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="wrapper-card">
+                <div className="btn">OK</div>
+                <Card heading='Nir' bio='lorem ipsum sit amet dolor' tag='This is a tag'/>
+                <Card heading='Jenny' bio='lorem ipsum sit amet dolor' tag='This is a tag 2'/>
+                <Card heading='Tomer' bio='lorem ipsum sit amet dolor' tag='This is a tag 3'/>
+                <Card heading='Eran' bio='lorem ipsum sit amet dolor' tag='This is a tag'/>
+                <Card heading='Maayan' bio='lorem ipsum sit amet dolor' tag='This is a tag'/>
+                <button>OK2</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
-
-
-
-
-
