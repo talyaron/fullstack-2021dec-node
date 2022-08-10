72dfe1d4965d42d4d4e79906449d8ac44a2b8e3c
Bella Brail <bellabrailovsky@gmail.com>
2022-08-10 21:04:05 +0300

    ...

--- 02-React/01-components/followups/Adam-Bella/my-app/src/App.tsx
+++ 02-React/01-components/followups/Adam-Bella/my-app/src/App.tsx
@@ -7,13 +7,12 @@ function App() {
     return (
         <div className="App">
             <div className="wrapper-card">
-                <div className="btn">OK</div>
                 <Card heading='Nir' bio='lorem ipsum sit amet dolor' tag='This is a tag'/>
                 <Card heading='Jenny' bio='lorem ipsum sit amet dolor' tag='This is a tag 2'/>
                 <Card heading='Tomer' bio='lorem ipsum sit amet dolor' tag='This is a tag 3'/>
                 <Card heading='Eran' bio='lorem ipsum sit amet dolor' tag='This is a tag'/>
                 <Card heading='Maayan' bio='lorem ipsum sit amet dolor' tag='This is a tag'/>
-                <button>OK2</button>
+                <Card heading='Udi' bio='lorem ipsum sit amet dolor' tag='This is a tag'/>
             </div>
         </div>
     );
