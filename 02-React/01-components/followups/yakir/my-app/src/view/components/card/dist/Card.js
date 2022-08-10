024dd31bbbc35faaa1c6f4d4de9d2ad98b2df110
YakirD <38473489+yakirdv@users.noreply.github.com>
2022-08-10 20:12:25 +0300

    react

---
+++ 02-React/01-components/followups/yakir/my-app/src/view/components/card/dist/Card.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var Card = function (_a) {
+    var src = _a.src;
+    return (React.createElement("img", { className: 'card', alt: "img", src: src }));
+};
+exports["default"] = Card;
