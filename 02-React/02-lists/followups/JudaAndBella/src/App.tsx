accf217135493762ba1a2ddea4e1cf815d8d3b68
Bella Brail <bellabrailovsky@gmail.com>
2022-08-17 18:31:41 +0300

    ...

--- 02-React/02-lists/followups/JudaAndBella/src/App.tsx
+++ 02-React/02-lists/followups/JudaAndBella/src/App.tsx
@@ -1,80 +1,82 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./view/styles/app.scss";
 
 import Card from "./view/components/card/Card";
 
 interface Breed {
-    breed : string,
-    country : string,
-    origin : string,
-    coat : string,
-    pattern : string
+  breed: string,
+  country: string,
+  origin: string,
+  coat: string,
+  pattern: string,
+
 }
+
 
 let factVar = "";
 
 function App() {
-    const [counter,
-        setCounter] = useState(0); //initial value
-    const [factVar,
-        setFactVar] = useState(''); //initial value
-    const [allBreeds,
-        setBreeds] = useState([]);
+  const [counter, setCounter] = useState(0); //initial value
+  const [factVar, setFactVar] = useState(''); //initial value
+  const [allBreeds, setBreeds] = useState([]);
 
-    async function handleAddCounter() {
-        try {
-            console.log("counter:", counter);
+  async function handleAddCounter() {
+    try {
+      console.log("counter:", counter);
 
-            const {data} = await axios.get("https://catfact.ninja/breeds"); //rest API
+      const { data } = await axios.get("https://catfact.ninja/breeds"); //rest API
 
-            const allBreeds = data.data
-            console.log(allBreeds);
+      const allBreeds = data.data
+      console.log(allBreeds);
+      
+      if (!allBreeds) throw new Error("No breeds");
 
-            if (!allBreeds) 
-                throw new Error("No breeds");
-            
-            setBreeds(allBreeds)
-            setCounter(counter + 1);
+      setBreeds(allBreeds)
+      setCounter(counter + 1);
 
-        } catch (error) {
-            console.error(error);
-        }
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  const counterEffect = counter;
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setCounter(counterEffect)
+    }, 300);
+    console.log("EFFECT RUNNING");
+
+    return () => {
+      console.log("EFFECT CLEANUP");
+      clearTimeout(identifier);
     };
+  }, [counterEffect]);
+  
+  
+  // useEffect(() => {
+  //   console.log("On useEffect")
+  //   handleAddCounter();
+  // },[]);
+  
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={handleAddCounter}>ADD</button>
+ 
+        <div className="btn">OK</div>
+        
+        {allBreeds.map((breed: Breed) => (
+          <Card key={breed.breed} breed={breed.breed} country={breed.country} origin={breed.origin} coat={breed.coat} pattern={breed.pattern} />
+        ))}
 
-    const counterEffect = counter;
-
-    useEffect(() => {
-        const identifier = setTimeout(() => {
-            setCounter(counterEffect)
-        }, 300);
-        console.log("EFFECT RUNNING");
-
-        return () => {
-            console.log("EFFECT CLEANUP");
-            clearTimeout(identifier);
-        };
-    }, [counterEffect]);
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <button onClick={handleAddCounter}>ADD</button>
-
-                <div className="btn">OK</div>
-
-                {allBreeds.map((breed : Breed) => (<Card
-                    key={breed.breed}
-                    breed={breed.breed}
-                    country={breed.country}
-                    origin={breed.origin}
-                    coat={breed.coat}
-                    pattern={breed.pattern}/>))}
-
-                <button>OK2</button>
-            </header>
-        </div>
-    );
+        <button>OK2</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
