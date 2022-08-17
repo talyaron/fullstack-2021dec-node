e2c2f7f86317236ccd87e9eee6234597dc7f637a
Tal Yaron <tal.yaron@gmail.com>
2022-08-17 18:03:22 +0300

    useEffect

--- 02-React/03-useEffect/example/src/App.tsx
+++ 02-React/03-useEffect/example/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./view/styles/app.scss";
 
@@ -20,7 +20,7 @@ const names: Name[] = [
   { id: "ryerhf", title: "Archimedes", text: "EUREKA!" },
 ];
 
-let factVar = "";
+
 
 interface Breed {
   breed: string;
@@ -29,16 +29,24 @@ interface Breed {
   coat: string;
   pattern: string;
 }
-
+let x = 1;
 function App() {
+  console.log("start function", x);
+  x++;
+
   const [counter, setCounter] = useState<number>(0); //initial value
   const [factVar, setFactVar] = useState<string>(""); //initial value
   const [breeds, setBreeds] = useState<Breed[]>([]);
 
+  useEffect(()=>{
+    console.log('run with use effect')
+    handleAddCounter();
+  },[])
+
   async function handleAddCounter() {
     try {
       // setCounter(counter + 1) //new value ... it will take some millisconds to update
-      console.log("counter:", counter);
+    
 
       const { data } = await axios.get("https://catfact.ninja/breeds"); //rest API
       if (!data) throw new Error("No data");
@@ -46,7 +54,7 @@ function App() {
       // if (!fact) throw new Error("No fact");
       // setFactVar(fact);
       console.log(data);
-      if(!('data' in data)) throw new Error('no data of breeds')
+      if (!("data" in data)) throw new Error("no data of breeds");
       setBreeds(data.data);
     } catch (error) {
       console.error(error);
@@ -56,7 +64,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={handleAddCounter}>ADD</button>
+        <button onClick={handleAddCounter}>Get</button>
+        <button onClick={()=>{setCounter(counter + 1)}}>ADD To Counter</button>
         <h3>{factVar}</h3>
         <h3>{counter}</h3>
         <div className="btn">OK</div>
