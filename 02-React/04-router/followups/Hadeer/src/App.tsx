83ec78f2eb8ad42bb0a5ee6dc28e38e248747819
hadeer57 <hadeer57@hotmail.com>
2022-08-17 20:32:07 +0300

    followup

--- 02-React/04-router/followups/Hadeer/src/App.tsx
+++ 02-React/04-router/followups/Hadeer/src/App.tsx
@@ -6,19 +6,33 @@ import {
 // import your route components too
 
 import Home from "./view/pages/home/Home";
-import About from "./view/pages/about/About";
+import Rose from "./view/pages/rose/Rose";
+import Lilly from "./view/pages/lilly/Lilly";
+import Main from "./view/pages/main/Main";
+import Lotus from "./view/pages/lotus/Lotus";
+
+import './App.css'
+
 
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </BrowserRouter>
-  )
-}
+      <Routes>
+        <Route path="/" element={<Main />}>
+          {/* OUTLET */}
+
+          <Route index element={<Home />} />
+          <Route path='rose' element={<Rose />} />
+          <Route path="lilly" element={<Lilly />} />
+          <Route path="lotus" element={<Lotus />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
 
 export default App
 
