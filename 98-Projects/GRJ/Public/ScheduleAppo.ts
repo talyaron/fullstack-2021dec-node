73310e8769688851290406bf2750de144126897a
Juda <Yehuda6993@gmail.com>
2022-08-17 17:39:59 +0300

    1

--- 98-Projects/GRJ/Public/ScheduleAppo.ts
+++ 98-Projects/GRJ/Public/ScheduleAppo.ts
@@ -1,93 +1,94 @@
-+
-
-
 
 
 function getUserId(): string | false {
-    try {
-      const queryString = window.location.search;
-      const urlParams = new URLSearchParams(queryString);
-      const userId = urlParams.get("userId");
-      console.log(userId)
-      return userId;
-    } catch (error) {
-      console.error(error);
-      return false;
-    }
+  try {
+    const queryString = window.location.search;
+    console.log(queryString);
+    
+    const urlParams = new URLSearchParams(queryString);
+    console.log(urlParams);
+
+    const userId = urlParams.get("userId");
+    console.log(userId)
+    return userId;
+  } catch (error) {
+    console.error(error);
+    return false;
   }
+}
 
-  function handleSelectDoctorType(ev){
-    ev.preventDefault()
-console.log(ev.target.id)
-const doctorType = ev.target.id;
+function handleSelectDoctorType(ev) {
+  ev.preventDefault()
+  console.log(ev.target.id)
+  const doctorType = ev.target.id;
 
-const searchBox = document.querySelector("#searchBox");
+  const searchBox = document.querySelector("#searchBox");
 
-let html = ` <br> <form onsubmit="handleSchedule(event)">
+  let html = ` <br> <form onsubmit="handleSchedule(event)">
 <input type="date" name="date" >
 <input type="hidden" name="doctorType" id=${doctorType}>
 <button type="submit">Search</button>
 
 </form>`
 
-searchBox.innerHTML = html
+  searchBox.innerHTML = html
 
-  }
+}
 
 async function handleSchedule(ev) {
-    ev.preventDefault()
-    console.log()
-    const date: string = ev.target.date.value;
-    const doctorType: string = ev.target.doctorType.id;
+  ev.preventDefault()
+  console.log()
+  const date: string = ev.target.date.value;
+  const doctorType: string = ev.target.doctorType.id;
 
-    console.log(date, doctorType)
-    // @ts-ignore
-    const { data } = await axios.post("/appo/getAppo", { doctorType, date })
+  console.log(date, doctorType)
+  // @ts-ignore
+  const { data } = await axios.post("/appo/getAppo", { doctorType, date })
 
-    const filteredAppos = data
-    console.log(filteredAppos)
-    renderAppo(filteredAppos)
+  const filteredAppos = data
+  console.log(filteredAppos)
+  renderAppo(filteredAppos)
 
 }
 
 
 
 function renderAppo(apposArray) {
-console.log(apposArray)
-    const filteredAppo: HTMLDivElement = document.querySelector('#filteredAppo');
-    let html = ""
+  console.log(apposArray)
+  const filteredAppo: HTMLDivElement = document.querySelector('#filteredAppo');
+  let html = ""
 
 
-    apposArray.forEach(appo => {
+  apposArray.forEach(appo => {
 
-        html += `<button id="${appo._id}" onclick="handlePickAppoTime(event)">${appo.time}</button>`
-        // html += `date: ${appo.date} <br>
-        // doctorType: ${appo.doctorType} <br>
-        // doctorId: ${appo.doctorId} <br>
-        // time: ${appo.time} <br>
-        // `
-    });
+    html += `<button id="${appo._id}" onclick="handlePickAppoTime(event)">${appo.time}</button>`
+    // html += `date: ${appo.date} <br>
+    // doctorType: ${appo.doctorType} <br>
+    // doctorId: ${appo.doctorId} <br>
+    // time: ${appo.time} <br>
+    // `
+  });
   console.log(apposArray[0])
-    filteredAppo.innerHTML = html
+  filteredAppo.innerHTML = html
 
 }
 
 function handlePickAppoTime(ev) {
 
-const appoId = ev.path[0].id;
-const userId = getUserId()
-console.log(userId)
-console.log(ev.path[0].id)
+  const appoId = ev.path[0].id;
+  const userId = getUserId()
+  console.log(userId)
+  console.log(ev.path[0].id)
 
-pairAppoToUser(userId, appoId)
+  pairAppoToUser(userId, appoId)
 }
 
 
-async function pairAppoToUser(userId, appoId){
- // @ts-ignore
-    const { data} = await axios.put("/appo/pairAppoToUser", {userId, appoId})
-    const {pairedAppo, error} = data
-    console.log(data)
+async function pairAppoToUser(userId, appoId) {
+  // @ts-ignore
+  const { data } = await axios.put("/appo/pairAppoToUser", { userId, appoId })
+  const { pairedAppo, error } = data
+  console.log(data)
 
 }
 
--- 98-Projects/GRJ/Public/dist/ScheduleAppo.js
+++ 98-Projects/GRJ/Public/dist/ScheduleAppo.js
@@ -34,7 +34,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-+function getUserId() {
+function getUserId() {
     try {
         var queryString = window.location.search;
         var urlParams = new URLSearchParams(queryString);
