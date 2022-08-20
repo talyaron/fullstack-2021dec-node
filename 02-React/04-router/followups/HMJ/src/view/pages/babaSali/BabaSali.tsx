30ddd04cac02ee329dfa45f87b6807c9ad08f43f
Juda <Yehuda6993@gmail.com>
2022-08-20 12:43:07 +0300

    12

---
+++ 02-React/04-router/followups/HMJ/src/view/pages/babaSali/BabaSali.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const BabaSali = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
+};
+
+export default About;
