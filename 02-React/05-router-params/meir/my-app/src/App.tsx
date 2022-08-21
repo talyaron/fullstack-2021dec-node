3bc3cb5873a348cb6921f99d8676694781dcc350
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-08-21 19:17:44 +0300

    WIP

---
+++ 02-React/05-router-params/meir/my-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Main from "./view/pages/main/Main";
+import Home from "./view/pages/home/Home";
+import Products from "./view/pages/products/Products";
+import Profile from "./view/pages/profile/Profile";
+import Page404 from "./view/pages/404/404";
+
+import "./view/styles/app.scss";
+import Product from "./view/pages/product/Product";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
