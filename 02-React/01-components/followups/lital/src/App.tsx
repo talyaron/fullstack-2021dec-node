c68a3c862e31eefce9f3695002b757f887cb707b
Lital Cohen <litalcohen479@gmail.com>
2022-08-22 18:21:59 +0300

    react

---
+++ 02-React/01-components/followups/lital/src/App.tsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./view/styles/app.scss";
+
+// import Card from "./view/components/card/Card";
+
+interface Breed {
+  breed: string;
+  country: string;
+  origin: string;
+  coat: string;
+  pattern: string;
+}
+let x = 1;
+function App() {
+  console.log("start function", x);
+  x++;
+
+  const [counter, setCounter] = useState<number>(0); //initial value
+  // const [factVar, setFactVar] = useState<string>(""); //initial value
+  const [breeds, setBreeds] = useState<Breed[]>([]);
+
+  useEffect(()=>{
+    console.log('run with use effect')
+    handleAddCounter();
+  },[])
+
+  async function handleAddCounter() {
+    try {
+      // setCounter(counter + 1) //new value ... it will take some millisconds to update
+    
+
+      const { data } = await axios.get("https://catfact.ninja/breeds"); //rest API
+      if (!data) throw new Error("No data");
+      // const { fact } = data;
+      // if (!fact) throw new Error("No fact");
+      // setFactVar(fact);
+      console.log(data);
+      if (!("data" in data)) throw new Error("no data of breeds");
+      setBreeds(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={handleAddCounter}>Get</button>
+        <button onClick={()=>{setCounter(counter + 1)}}>ADD To Counter</button>
+        {/* <h3>{factVar}</h3> */}
+        <h3>{counter}</h3>
+        <div className="btn">OK</div>
+        {/* {names.map((name: Name) => (
+          <Card key={name.id} title={name.title} text={name.text} />
+        ))} */}
+        {breeds.map((breed) => (
+          <p key={breed.breed}>{breed.breed}</p>
+        ))}
+
+        <button>OK2</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-React/01-components/followups/lital/src/view/components/card/Card.tsx
@@ -0,0 +1,18 @@
+interface CardProps{
+    text:string;
+    title:string;
+    img:string;
+}
+
+const Card =({text, title, img}:CardProps) =>{
+  return (
+    <div  className='card'>
+        <p>{title}</p>
+        <h2>{text}</h2> 
+        <img src={img} alt="" />
+    </div>
+
+  )
+}
+
+export default Card
