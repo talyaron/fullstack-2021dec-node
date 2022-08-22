610483166dd699e26aeda6939ffeca5e0ad50d02
Gal <galgross24@gmail.com>
2022-08-22 16:17:45 +0300

    שדג

--- 02-React/05-router-params/Dogs/project-dogs/Gal/my-app/src/App.tsx
+++ 02-React/05-router-params/Dogs/project-dogs/Gal/my-app/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Page404 from "./features/404/404";
+import "./App.css";
+import Breeds from "./features/breeds";
+import Breed from "./features/breed";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Page404 />} />
+        <Route path="/" element={<Breeds />} />
+        <Route path="/breed/:breed" element={<Breed />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
