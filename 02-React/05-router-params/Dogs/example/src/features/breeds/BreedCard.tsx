5d4c23bd4a979728c6629459e64aa5e94a2089db
Tal Yaron <tal.yaron@gmail.com>
2022-08-24 18:43:11 +0300

    Example

--- 02-React/05-router-params/Dogs/example/src/features/breeds/BreedCard.tsx
+++ 02-React/05-router-params/Dogs/example/src/features/breeds/BreedCard.tsx
@@ -1,7 +1,19 @@
-const BreedCard = () => {
-  return (
-    <div>BreedCard</div>
-  )
-}
+import { FC } from "react";
+import { Image } from "./getBreeds";
+import { Link } from "react-router-dom";
 
-export default BreedCard
+interface BreedCardProps {
+  breed: Image;
+}
+const BreedCard: FC<BreedCardProps> = ({ breed }) => {
+  return (
+    <Link to={`/breed/${breed.breed}`}>
+      <div className="card">
+        <img src={breed.src} alt={`${breed.breed}`} />
+        <p>{breed.breed}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default BreedCard;
--- 02-React/05-router-params/Dogs/example/src/features/breeds/index.tsx
+++ 02-React/05-router-params/Dogs/example/src/features/breeds/index.tsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { getBreeds } from "./getBreeds";
+import BreedCard from "./BreedCard";
+import { getBreeds, Image } from "./getBreeds";
+import './breeds.scss';
 
 const Breeds = () => {
   //set state breeds
-  const [breeds, setBreeds] = useState<string[]>([]);
+  const [breeds, setBreeds] = useState<Image[]>([]);
 
   useEffect(() => {
-    getBreeds().then((brds) => {
-      if (brds) {
-        console.log(brds)
+    getBreeds().then((brds: Image[]) => {
+      if (brds.length > 0) {
+        console.log(brds);
         setBreeds(brds);
       }
     });
   }, []);
 
-  return <div>Breeds</div>;
+  return (
+    <div>
+      <h1>Breeds</h1>
+      {breeds.map((breed, i)=><BreedCard breed={breed}/>)}
+    </div>
+  );
 };
 
 export default Breeds;
