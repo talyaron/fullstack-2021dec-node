ccbd4afd197dceef9dd5057773c68034de5e487b
Tal Yaron <tal.yaron@gmail.com>
2022-08-28 18:59:25 +0300

    cont

--- 02-React/07-conditional-rendering/exercise/Adam-Dorit/pictures/src/App.tsx
+++ 02-React/07-conditional-rendering/exercise/Adam-Dorit/pictures/src/App.tsx
@@ -4,34 +4,27 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [counter,setcounter] = useState<number>(0)
+  const [isTrue,setIsTrue] = useState<boolean>(false)
+  
   function addCounter(){
-    setcounter( counter+1 )
-    console.log(counter)
+   
+    setIsTrue( !isTrue );
+    console.log(isTrue)
   }
   return (
     <div className="App">
       <header className="App-header">
-        {counter % 2 === 0 ? (
-          <img onClick={addCounter}
-            src="https://media1.giphy.com/media/26gsasKHkeH0VP8d2/200w.gif?cid=82a1493bairztvk1m6qm17y5df3ii6puqubjvw1adwzyk7ul&rid=200w.gif&ct=g"
-            className="App-logo"
-            alt="logo"
+        {isTrue ? (
+          <div onClick={addCounter}
+            // src="https://media1.giphy.com/media/26gsasKHkeH0VP8d2/200w.gif?cid=82a1493bairztvk1m6qm17y5df3ii6puqubjvw1adwzyk7ul&rid=200w.gif&ct=g"
+            className="div"
+            // alt="logo"
           />
         ) : (
-          <img onClick={addCounter} src={logo} className="App-logo" alt="logo" />
+          <div onClick={addCounter}  className="red"  />
         )}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        
       </header>
     </div>
   );
