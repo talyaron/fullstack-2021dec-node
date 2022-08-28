0ca70f86cfc618faed6591b5e3a5444592b5cb75
Tal Yaron <tal.yaron@gmail.com>
2022-08-28 18:18:49 +0300

    Toggle images

--- 02-React/07-conditional-rendering/example/src/App.tsx
+++ 02-React/07-conditional-rendering/example/src/App.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [counter, setCounter] = useState<number>(0);
+
+  function handleCheck(ev: any) {
+    try {
+      console.dir(ev.target.checked);
+      setIsChecked(ev.target.checked);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function handleAddCounter() {
+    try {
+      setCounter(counter + 1);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+        className="App-header"
+        style={{ backgroundColor: isChecked ? `green` : `red` }}>
+        {counter % 2 === 0 ? (
+          <img
+            src="https://media1.giphy.com/media/26gsasKHkeH0VP8d2/200w.gif?cid=82a1493bairztvk1m6qm17y5df3ii6puqubjvw1adwzyk7ul&rid=200w.gif&ct=g"
+            className="App-logo"
+            alt="logo"
+          />
+        ) : (
+          <img src={logo} className="App-logo" alt="logo" />
+        )}
+        <input type="checkbox" name="" id="" onChange={handleCheck} />
+        {isChecked ? <h2>Checked</h2> : <h2>Not Checked</h2>}
+        <h2>{counter}</h2>
+        <button onClick={handleAddCounter}>ADD</button>
       </header>
     </div>
   );
---
+++ 02-React/07-conditional-rendering/exercise/start.md
@@ -0,0 +1,4 @@
+create a page with an image. 
+after clicking the image, it will shift to another image.
+on the following click, the image changes to the first image... etc.
+Toggle images 
