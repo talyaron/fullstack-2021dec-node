a393141ae3f8995776d6504fec0719e2a3a6cff2
Tal Yaron <tal.yaron@gmail.com>
2022-08-28 20:33:00 +0300

    cont - hw

--- 02-React/07-conditional-rendering/example/src/App.tsx
+++ 02-React/07-conditional-rendering/example/src/App.tsx
@@ -1,49 +1,18 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [isChecked, setIsChecked] = useState<boolean>(false);
-  const [counter, setCounter] = useState<number>(0);
+import Test from "./features/test/Test";
+import LoginRegister from "./features/LoginRegister/LoginRegister";
 
-  function handleCheck(ev: any) {
-    try {
-      console.dir(ev.target.checked);
-      setIsChecked(ev.target.checked);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  function handleAddCounter() {
-    try {
-      setCounter(counter + 1);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
+const App = () => {
   return (
-    <div className="App">
-      <header
-        className="App-header"
-        style={{ backgroundColor: isChecked ? `green` : `red` }}>
-        {counter % 2 === 0 ? (
-          <img
-            src="https://media1.giphy.com/media/26gsasKHkeH0VP8d2/200w.gif?cid=82a1493bairztvk1m6qm17y5df3ii6puqubjvw1adwzyk7ul&rid=200w.gif&ct=g"
-            className="App-logo"
-            alt="logo"
-          />
-        ) : (
-          <img src={logo} className="App-logo" alt="logo" />
-        )}
-        <input type="checkbox" name="" id="" onChange={handleCheck} />
-        {isChecked ? <h2>Checked</h2> : <h2>Not Checked</h2>}
-        <h2>{counter}</h2>
-        <button onClick={handleAddCounter}>ADD</button>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Test />} />
+        <Route path="/login" element={<LoginRegister />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
