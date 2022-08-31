634d3609119debd1216eed5f83eb388e1dcb46e4
Marina <garpalion@gmail.com>
2022-08-31 17:26:39 +0300

    update

--- 02-React/07-conditional-rendering/miniProject/marina/src/features/fact/Fact.tsx
+++ 02-React/07-conditional-rendering/miniProject/marina/src/features/fact/Fact.tsx
@@ -3,24 +3,30 @@ import { FactInterface } from '../factsList/FactsList'
 import { items } from "../factsList/FactsList";
 import { FactCard } from './FactCard'
 
+type FactProps = {
+  fact: {
+    src: string
+    text: string
+    id: number
+  }
+}
 
-
-export const Fact = () => {
+export const Fact = (props: FactProps) => {
   const { id } = useParams();
   
-  // const answer = getFact(id, items)
+  const answer = getFact(id, items)
 
   return (
     <div>
       <Link to="/list">Back</Link>
-      {/* <h1>Fact: {items ? items.title : ""} </h1> */}
+      <h1>Fact: {items ? items.text : ""} </h1>
     </div>
   );
 }
 
 
 
-function getFact(id: number, items: FactInterface[]): FactInterface | undefined {
+function getFact(id: number, fact: FactInterface[]): FactInterface | undefined {
   if (id) {
     return items.find((item) => item.id === id);
   } else {
