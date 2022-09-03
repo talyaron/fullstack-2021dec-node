e4c5ee2a4792eb99d346853360088460925877b9
Gili <gilimena95@gmail.com>
2022-09-03 11:49:01 +0300

    update

--- 02-React/05-router-params/Dogs/project-dogs/Gili/src/featuers/navBar/Nav.tsx
+++ 02-React/05-router-params/Dogs/project-dogs/Gili/src/featuers/navBar/Nav.tsx
@@ -1,14 +1,39 @@
 import { Link } from "react-router-dom";
+import { getAllBreeds } from "../breeds/getAllBreeds";
 import "./nav.scss"
+import { Image } from './../../../../../roy/src/features/breed/getimage';
 
 export const Nav = () => {
     return( 
     <div className="nav">
         <div className="nav__links">
         <Link to="/"><div className="nav__links__item">Home</div>{" "} </Link>
-        <div className="nav__links__item">Search breed</div>
+        <div className="nav__links__item">
+            <h3>Search Breed</h3>
+            <form onSubmit={handleFindBreed}>
+            <input name="search" type="text" />
+            <button type="submit">FIND</button>
+            </form>
+        </div>
         </div>
         <h1 className="header">Breeds</h1>
     </div>
     )
+}
+
+async function handleFindBreed(ev:any) {
+    ev.preventDefault()
+    try {
+        const breedsArr = await getAllBreeds();
+        const search = ev.target.search.value;
+        // const results:Array<Image> = [];
+        // breedsArr.forEach(breed => {
+        //     if(breed.breed === search) results.push(breed)
+        // })
+        const results = breedsArr.filter(brd => brd.breed.includes(ev.target.search.value) )
+        console.log(results)
+        return results;
+    } catch (error) {
+        console.log(error)
+    }
 }
