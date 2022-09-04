d68e7196195f908abcb3b02f4bf9ea0f95740250
Gal <galgross24@gmail.com>
2022-09-04 16:57:49 +0300

    finish, fix css

--- 02-React/07-conditional-rendering/miniProject/GAL/my-app/client/src/features/personalFacts/OptionCard.tsx
+++ 02-React/07-conditional-rendering/miniProject/GAL/my-app/client/src/features/personalFacts/OptionCard.tsx
@@ -1,6 +1,5 @@
-import { FC } from 'react'
 import {Option} from '../personalFacts/factModel'
-
+import './PersonalFacts.scss'
 
 interface OptionCardProps {
   option: Option;
@@ -8,13 +7,12 @@ interface OptionCardProps {
 
 
 
-export const OptionCard: FC<OptionCardProps> = (props) => {
-  const { option } = props;
+export const OptionCard = ({option}: OptionCardProps)  => {
 
   return (
     <div >
       <div className="flex__card">
-        <img className="flex__card__img" src={option.src} alt="img" />
+        <img className="flex__card__img" src={option.src} alt="img" id={option.id}/>
         <p className="flex__card__text">{option.text}</p>
       </div>
     </div>
