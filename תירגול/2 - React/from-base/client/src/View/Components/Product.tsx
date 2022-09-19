9eeab4b58023ee39156598621dd356868f222717
Bella Brail <bellabrailovsky@gmail.com>
2022-09-19 21:56:04 +0300

    ...

--- תירגול/2 - React/from-base/client/src/View/Components/Product.tsx
+++ תירגול/2 - React/from-base/client/src/View/Components/Product.tsx
@@ -9,6 +9,9 @@ interface ProductProps {
 
 const ProductComponent = ({ item, index, setNumber }: ProductProps) => {
   const [colorUser, setColorUser] = useState<string>("red");
+  let [quantity, setQuantity] = useState<number>(item.quantity);
+  let [productName, setProductName] = useState<string>(item.name);
+
 
   function changeColorMine(colorMine: string) {
     setColorUser(colorMine);
@@ -17,18 +20,37 @@ const ProductComponent = ({ item, index, setNumber }: ProductProps) => {
   function changeColorBlue() {
     setColorUser("blue");
   }
+  
+  function addQuantity() {
+    setQuantity(++quantity);
+  }
+  
+  function minusQuantity() {
+    setQuantity(--quantity);
+  }
+  
+  // function changeName() {
+  //   letproductNames = 
+  // }
+
 
   return (
     <div key={index} className="productItem" style={{ color: colorUser }}>
       <p>{item.name}</p>
       <p>{item.price}</p>
+      <p>{quantity}</p>
+      {/* <input type="text" onBlur={changeName} /> */}
       <button onClick={() => changeColorMine("green")}>
         Change me to anything!!!
       </button>
       <button onClick={changeColorBlue}>Change me to blue!!!</button>
 
-      <button onClick={() => setNumber(Math.round(Math.random() * 10))}>
-        Change number
+      <button onClick={addQuantity}>
+        Add
+      </button>
+      
+      <button onClick={minusQuantity}>
+        Delete
       </button>
     </div>
   );
--- תירגול/2 - React/from-base/client/src/data/products.ts
+++ תירגול/2 - React/from-base/client/src/data/products.ts
@@ -2,11 +2,12 @@
 export interface Product {
     id: string,
     name: string,
-    price: number
+    price: number,
+    quantity: number
 }
 
 export const products: Array<Product> = [
-    { id: "1qwe", name: "Orange", price: 123 },
-    { id: "sd123123wf", name: "Apple", price: 234 },
-    { id: "324234", name: "Banana", price: 345 },
+    { id: "1qwe", name: "Orange", price: 123, quantity: 1 },
+    { id: "sd123123wf", name: "Apple", price: 234, quantity: 1 },
+    { id: "324234", name: "Banana", price: 345, quantity: 1 }
 ]
