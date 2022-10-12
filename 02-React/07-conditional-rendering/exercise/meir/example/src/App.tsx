d91d847e706a2530a46e4f8b32c602208fd79338
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-10-12 03:15:00 +0300

    conditional-rendering.example/wip

---
+++ 02-React/07-conditional-rendering/exercise/meir/example/src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+   const [isChecked, setIsChecked] = useState<boolean>(false)
+   const [counter, setCounter] =useState<number>(0);
+   function handleCheck(ev:any) {
+     try {
+      console.dir(ev.target.checked);
+      setIsChecked(ev.target.checked);
+     } catch (error) {
+       console.error(error)
+
+     }
+  }
+
+  function handleAddCounter(ev:any) {
+    try {
+      setCounter(counter+1)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+    return (
+      <div className="App">
+        <header className="App-header" style={{backgroundColor:isChecked?`yellow`:`navy`}}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <input type="checkbox" name="" id="" onChange={handleCheck}/>
+          {isChecked?<h2>Checked</h2>:<h2>Not Checked</h2>}
+          <h2>{counter}</h2>
+          <button onClick={handleAddCounter}>ADD</button>
+        </header>
+      </div>
+    );
+    
+  
+}
+
+export default App;
