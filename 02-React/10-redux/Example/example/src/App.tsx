88b50f0e84bd43926eb347ac646a3b574ceff43a
Tal Yaron <tal.yaron@gmail.com>
2022-10-26 18:51:51 +0300

    example

--- 02-React/10-redux/Example/example/src/App.tsx
+++ 02-React/10-redux/Example/example/src/App.tsx
@@ -1,58 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { changeText, textSelector } from "./features/text/textSlice";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Input />
+      <Output />
     </div>
   );
 }
 
 export default App;
+
+function Input() {
+  const dispatch = useAppDispatch();
+  function handleTextChange(ev: any) {
+    dispatch(changeText(ev.target.value));
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handleTextChange} />
+    </div>
+  );
+}
+
+function Output() {
+  const text = useAppSelector(textSelector)
+  return <div>{text.length>0?text:"Text goes here"}</div>;
+}
