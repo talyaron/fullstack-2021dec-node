9db0c448240742a24956490066cc0a95b2f9e7b7
Tal Yaron <tal.yaron@gmail.com>
2022-10-26 20:24:18 +0300

    WIP

--- 02-React/10-redux/Example/example/src/app/store.ts
+++ 02-React/10-redux/Example/example/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import blogSlice from '../features/blog/blogSlice';
 import counterReducer from '../features/counter/counterSlice';
 import textSlice from '../features/text/textSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    text:textSlice
+    text:textSlice,
+    blog:blogSlice
   },
 });
 
