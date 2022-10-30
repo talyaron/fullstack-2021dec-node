1b7915fc86a7c44a5e4ccbac6f3ebce23dac46be
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-10-31 01:26:31 +0200

    redux in classroom/wip

---
+++ 02-React/10-redux/followUp-JGH/meirushka/my-app/src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { changeText, textSelector } from './features/counter/text/textSlice';
+import { getMyJoke } from "./features/counter/text/textAPI";
+
+
+function App() {
+
+  const status = useAppSelector(statusSelector)
+  const dispatch = useAppDispatch()
+
+  function handleGetMyJoke() {
+    try {
+      dispatch(getMyJoke())
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  return (
+    <div>
+      <input />
+      <button onClick={handleGetMyJoke}>Get My Joke</button>
+    </div>
+  );
+}
+
+export default App;
+
+function Input() {
+  const dispatch = useAppDispatch();
+  function handleTextChange(ev: any) {
+    dispatch(changeText(ev.target.value));
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handleTextChange} />
+    </div>
+  )
+}
+
+function Output() {
+  const text = useAppSelector(textSelector)
+  return <div>{text.length>0?text:"Text goes here"}</div>
+}
