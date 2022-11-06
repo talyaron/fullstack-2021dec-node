4262370e614eb31f2a930ed3eb599a845a482b46
Meir Duvid Krykun <97189724+Rebmeir770@users.noreply.github.com>
2022-11-06 17:37:11 +0200

    redux-dogs-wip

---
+++ 02-React/05-router-params/Meir-Dogs/my-app/src/app/store.ts
@@ -0,0 +1,19 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {},
+})
+
+// // Infer the `RootState` and `AppDispatch` types from the store itself
+// export type RootState = ReturnType<typeof store.getState>
+// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+// export type AppDispatch = typeof store.dispatch
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
