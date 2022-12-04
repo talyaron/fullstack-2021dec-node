b71797723758992ed0bab3dc33987415b445845e
Marina <garpalion@gmail.com>
2022-12-04 23:57:09 +0200

    del

--- marinDB/client/src/models/postInterface.ts
+++
@@ -1,9 +0,0 @@
-export default interface PostInterface {
-  id: number | null;
-  userId: number | null;
-  name: string;
-  img: string;
-  profilePic: string;
-  description: string;
-  createdAt?: Date;
-}
--- marinDB/client/src/userRedux/joiModel.ts
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi'
-
-export const UserValidation = Joi.object({
-  // username: Joi.string().allow(null, ""),
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-});
